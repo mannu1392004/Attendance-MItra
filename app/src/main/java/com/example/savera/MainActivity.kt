30ff1cb66f9b6973f999309619278f28fd7919db
Lakshay Dureja <lakshaydureja7@gmail.com>
2024-03-15 23:45:14 +0530

    Added year input for 1st time account logins and save there year in firestore

--- app/src/main/java/com/example/savera/MainActivity.kt
+++ app/src/main/java/com/example/savera/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
 
 
-Navigation()
+        Navigation()
 
                 }
             }
